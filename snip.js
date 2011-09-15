f3fb79971f1b192ea515b426f712b18c7cd6dd11
Piotr Petrus <riddle@aiur.(none)>
2011-09-15 18:32:04 +0200

    Checking if there’s no need to snip. Duh.

--- snip.js
+++ snip.js
@@ -6,27 +6,30 @@ $(function() {
         $snip_excerpt = $('<div class="snip-excerpt"></div>'),
         $snip_more = $('<a href="">More</a>'),
         $snip_less = $('<a href="">Less</a>'),
-        $whitespace = ' ',
+        whitespace = ' ',
+        text_length = $snip.text().length,
         snip_length = $snip.data('snip-length');
-        
-    $snip.wrapInner('<div class="snip-original"></div>');
-    
-    var $snip_original = $snip.find('.snip-original');
-    
-    console.log($snip_original);
-    
-    $snip_original.hide().append($whitespace, $snip_less);
-    $snip_excerpt
-      .append($snip_original.text().slice(0, snip_length))
-      .append('…')
-      .append($whitespace, $snip_more)
-      .appendTo($snip);
+            
+    if (text_length > snip_length) {
       
-    $snip_more.add($snip_less).bind('click', function(e) {
-      $snip_original.toggle();
-      $snip_excerpt.toggle();
-      e.preventDefault();
-    });
+      $snip.wrapInner('<div class="snip-original"></div>');
+
+      var $snip_original = $snip.find('.snip-original');
+  
+      $snip_original.hide().append(whitespace, $snip_less);
+      $snip_excerpt
+        .append($snip_original.text().slice(0, snip_length))
+        .append('…')
+        .append(whitespace, $snip_more)
+        .appendTo($snip);
+    
+      $snip_more.add($snip_less).bind('click', function(e) {
+        $snip_original.toggle();
+        $snip_excerpt.toggle();
+        e.preventDefault();
+      });
+    
+    }
 
     
   });
